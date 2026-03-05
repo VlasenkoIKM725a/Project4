0ab2627df21e5f60a3d1e96bd65b7fa976f61278
Andrii Vlasenko <Andrii.Vlasenko@infiz.khpi.edu.ua>
2026-03-05 08:53:31 +0100

    Варінт 5 Завдання 2

---
+++ Project4/FileName.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int number = 663;
+    int count = 0;
+    const int limit = 20;
+
+    cout << "Перші 20 чисел (> 500), що діляться на 13 і 17:" << endl;
+    while (count < limit) {
+        cout << count + 1 << ") " << number << endl;
+        count++;
+        number += 221;
+    }
+
+    return 0;
+}
